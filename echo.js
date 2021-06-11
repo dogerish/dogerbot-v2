a8c46b47af82fe668420bbd173fbd4201dbf7be9
dogerish <luca.filmstrip@gmail.com>
2021-06-11 13:35:41 -0400

    Add an option to use a custom delimiter in `echo`

--- echo.js
+++ echo.js
@@ -1,10 +1,22 @@
-// echo command
 const BaseCmd = require("./basecmd.js");
+const GetOpt  = require("./getopt.js");
+const utils   = require("./utils.js");
 
+// echo command
 class EchoCmd extends BaseCmd
 {
-	call(/*Discord.Message*/ msg, /*String...*/ arg0, ...args)
-	{ msg.channel.send(args.join(' ') || "** **"); }
+	call(/*Discord.Message*/ msg, /*String...*/ ...args)
+	{
+		// get delimiter, default is a space
+		let delim = ' ';
+		const go = new GetOpt("d:delim:", args);
+		while (go.next())
+		{
+			if (go.opterr) return msg.channel.send(utils.ferr(args[0], go.opterr));
+			delim = go.optarg;
+		}
+		msg.channel.send(args.slice(go.optind).join(delim) || "** **");
+	}
 }
 
 module.exports = EchoCmd;
