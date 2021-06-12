ff7408877262e23e047ce1da4bd72c3be44c9289
dogerish <luca.filmstrip@gmail.com>
2021-06-12 14:28:54 -0400

    Add template for basic commands

---
+++ cmds/t_cmd.js
@@ -0,0 +1,14 @@
+const BaseCmd = require("../cmd-types/basecmd.js");
+
+class TemplateCmd extends BaseCmd
+{
+	// 0 on success
+	/*Number*/ call(/*Discord.Message*/ msg, /*String...*/ ...args)
+	{
+		if (super.call(msg, ...args)) return 1;
+		/* command body */
+		return 0;
+	}
+}
+
+module.exports = TemplateCmd;
