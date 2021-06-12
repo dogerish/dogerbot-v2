2b854864030e55ed2008b68824964dbd0d7f73b6
dogerish <luca.filmstrip@gmail.com>
2021-06-11 23:26:27 -0400

    Add guild and perm -specific command type

---
+++ cmd-types/guildcmd.js
@@ -0,0 +1,22 @@
+const BaseCmd = require("./basecmd.js");
+const utils   = require("../utils/utils.js");
+
+// only works in guilds/servers
+class GuildCmd extends BaseCmd
+{
+	// returns 0 on pass
+	/*Number*/ call(/*Discord.Message*/ msg, /*String*/ ...args)
+	{
+		if (super.call(msg, ...args)) return 1;
+		if (!["dm", "unknown"].includes(msg.channel.type)) return 0;
+		msg.channel.send(
+			utils.ferr(
+				args[0],
+				"This command needs to be used in a server."
+			)
+		);
+		return 1;
+	}
+}
+
+module.exports = GuildCmd;
---
+++ cmd-types/permcmd.js
@@ -0,0 +1,31 @@
+const GuildCmd = require("./guildcmd.js");
+const utils    = require("../utils/utils.js");
+
+class PermCmd extends GuildCmd
+{
+	constructor(/*Array(Discord.PermissionResolvable)*/ perms, /*Boolean*/ checkAdmin = true)
+	{
+		super();
+		this.perms = perms;
+		this.checkAdmin = checkAdmin;
+	}
+
+	// 0 on success
+	/*Number*/ call(/*Discord.Message*/ msg, /*String*/ ...args)
+	{
+		if (super.call(msg, ...args)) return 1;
+		if (this.perms.some(perm => msg.member.permissions.has(perm, this.checkAdmin)))
+			return 0;
+		msg.channel.send(
+			utils.ferr(
+				args[0],
+				  "You must have at least one of these permissions: `"
+				+ this.perms.join("`, `")
+				+ "`."
+			)
+		);
+		return 1;
+	}
+}
+
+module.exports = PermCmd;
