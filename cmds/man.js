1496fc38c1130632f7a061b21caed6885d13e9f9
dogerish <luca.filmstrip@gmail.com>
2021-06-19 12:22:14 -0400

    Add help and a default argument to `man`
    `help` as an alias to `man -f` for a brief desc.
    `man` will now default to its arg0 for the command
    argument.

--- cmds/man.js
+++ cmds/man.js
@@ -2,6 +2,7 @@ const BaseCmd = require("../cmd-types/basecmd.js");
 const fs      = require("fs");
 const utils   = require("../utils/utils.js");
 const cfg     = require("../config/cfg.json");
+const GetOpt  = require("../utils/getopt.js");
 
 class ManCmd extends BaseCmd
 {
@@ -44,22 +45,37 @@ class ManCmd extends BaseCmd
 	/*Number*/ call(/*Discord.Message*/ msg, /*Array<String>*/ args)
 	{
 		if (super.call(msg, args)) return 1;
-		if (!args[1])
+		// get options
+		const go   = new GetOpt("f,whatis,", args);
+		let whatis = false;
+		while (go.next())
 		{
-			msg.channel.send(utils.ferr(args[0], "No command specified."));
-			return 1;
+			if (go.opterr)
+			{
+				msg.channel.send(utils.ferr(args[0], go.opterr));
+				return 1;
+			}
+			switch (go.opt) { case 'f': case "whatis": whatis = true; }
 		}
-		let [cmdname, cmd] = this.parser.deAliasCmd(args[1]);
+
+		// trim args down to omit options
+		args = [args[0], ...args.slice(go.optind)];
+		// default name to this command name and dealias it
+		let [cmdname, cmd] = this.parser.deAliasCmd(args[1] || args[0]);
+		// unknown command
 		if (!cmd)
 		{
 			msg.channel.send(utils.ferr(args[0], `Unknown command: \`${cmdname}\``));
 			return 1;
 		}
+		// construct the embed
 		let embed = { fields: [] }, curField;
 		let data;
+		// try reading and notify of failures
 		try { data = String(fs.readFileSync(cmd.manpage)).split('\n'); }
 		catch (e)
 		{
+			// file not there, undocumented (already logged this on startup)
 			if (e.code == "ENOENT")
 			{
 				msg.channel.send(utils.ferr(
@@ -68,49 +84,61 @@ class ManCmd extends BaseCmd
 				));
 				return 1;
 			}
+			// unexpected error, log it
 			console.error(`Error (${args[0]}, ${e.code}):`, e);
 			msg.channel.send(utils.ferr(
 				args[0],
 				`Internal error (code ${e.code}). Please make a bug report.`
 			));
 		}
+		// shortcut to sub the keys of a string in this context
 		let subit = str => ManCmd.subKeys(msg, cmdname, cmd, str);
+		// parse lines
 		for (let line of data)
 		{
 			switch (line[0])
 			{
+			// header/field name
 			case '#':
 				let pre = line.substr(1, 5);
 				switch (pre)
 				{
-				case "TITLE": embed.title       = subit(line.substr(7)); continue;
-				case "DESCR": embed.description = subit(line.substr(7)); continue;
+				case "TITLE": embed.title = subit(line.substr(7)); continue;
+				case "DESCR":
+					embed.description = subit(line.substr(7));
+					if (whatis)
+					{
+						msg.channel.send(embed.description || "<UNDEFINED>");
+						return 0;
+					}
+					continue;
 				default:
 					curField = embed.fields.push(
 						{ name: line.substr(1), value: "" }
 					) - 1;
 					continue;
 				}
-			case '\t':
-				let tabcount = line.match(/^\t+/);
-				    tabcount = tabcount && tabcount[0].length;
-				line = "|--> ".repeat(tabcount) + line.substr(tabcount);
-				break;
+			// replace all tabs since they aren't supported
+			case '\t': line = line.replace(/\t/g, "|--> "); break;
 			}
-			embed.fields[curField].value += subit(line) + '\n';
+			// add the line to the current field if both the current field is defined
+			// and line isn't empty
+			if (curField !== undefined && line.length)
+				embed.fields[curField].value += subit(line) + '\n';
 		}
+		// don't send empty field values
 		embed.fields.forEach((f, i) => {
-			if (!f.value) { f.value = "empty"; embed.fields[i] = f; }
+			if (!f.value) { f.value = "<UNDEFINED>"; embed.fields[i] = f; }
 		});
-		msg.channel.send({ embed: embed }).catch(e =>
-		{
+		// send the embed and log unexpected errors
+		msg.channel.send({ embed: embed })
+		.catch(e => {
 			console.error(`Error (${args[0]}, ${e.code}):`, e);
 			msg.channel.send(utils.ferr(
 				args[0],
-				`Internal error (code ${e.code}). Please make a bug report.`
+				`Internal error (code \`${e.code}\`). Please make a bug report.`
 			))
-		}
-		);
+		});
 		return 0;
 	}
 }
